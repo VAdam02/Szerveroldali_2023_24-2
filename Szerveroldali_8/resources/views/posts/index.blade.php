58ca737e14a8bfaf436b78f98774ef4407f07f4d
VAdam02 <adam.vilanyi2@gmail.com>
2024-03-05 18:25:56 +0100

    Link to show post

--- Szerveroldali_8/resources/views/posts/index.blade.php
+++ Szerveroldali_8/resources/views/posts/index.blade.php
@@ -1,1 +1,8 @@
 <p>Hello world</p>
+
+@foreach ($posts as $post)
+<a href="{{ route('posts.show', $post->id) }}">
+    <h2>{{ $post->title }}</h2>
+    <p>{{ $post->content}}</p>
+</div>
+@endforeach
