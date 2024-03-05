30c7f1dfd75403dabe22bc0da608db586fcae178
VAdam02 <adam.vilanyi2@gmail.com>
2024-03-05 18:45:03 +0100

    Content formatted

--- Szerveroldali_8/resources/views/posts/index.blade.php
+++ Szerveroldali_8/resources/views/posts/index.blade.php
@@ -5,7 +5,10 @@
     <div class="flex-auto w-96 p-4 shadow-md rounded-lg bg-gray-200">
         <a href="{{ route('posts.show', $post->id) }}">
             <h2 class="truncate font-semibold text-lg mb-2">{{ $post->title }}</h2>
-            <p>{{ $post->content}}</p>
+            <div class="h-24 overflow-hidden relative">
+                <div class="absolute bottom-0 left-0 w-full h-20 bg-gradient-to-t from-gray-200 to-transparent"></div>
+                <p>{{ $post->content}}</p>
+            </div>
         </a>
     </div>
     @endforeach
