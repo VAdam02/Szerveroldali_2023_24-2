dd816fd945a7d4fafd81dd138b3489cea9327ed8
VAdam02 <adam.vilanyi2@gmail.com>
2024-03-12 18:46:00 +0100

    General layout php artisan make:component GeneralLayout

--- Szerveroldali_8/resources/views/posts/index.blade.php
+++ Szerveroldali_8/resources/views/posts/index.blade.php
@@ -1,5 +1,7 @@
 @vite(['resources/css/app.css','resources/js/app.js'])
 
+<x-general-layout title="Posts">
+
 @include('posts.partials.carouser')
 
 <div class="my-3 gap-10 flex flex-wrap">
@@ -7,3 +9,5 @@
     @include('posts.partials.card')
     @endforeach
 </div>
+
+</x-general-layout>
