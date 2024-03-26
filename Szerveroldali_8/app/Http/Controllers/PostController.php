3d85bf4cb100a1f6dc88860e35290116fba7894f
VAdam02 <adam.vilanyi2@gmail.com>
2024-03-26 18:39:28 +0100

    Self defensing controller

--- Szerveroldali_8/app/Http/Controllers/PostController.php
+++ Szerveroldali_8/app/Http/Controllers/PostController.php
@@ -10,6 +10,11 @@ use App\Models\Category;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
     /**
      * Display a listing of the resource.
      */
--- Szerveroldali_8/routes/web.php
+++ Szerveroldali_8/routes/web.php
@@ -40,11 +40,10 @@ Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.de
 */
 
 Route::resource('users', UserController::class);
-//Route::resource('posts', PostController::class)->middleware('auth');
+Route::resource('posts', PostController::class);
 Route::get('/post2/{id}/edit', [PostController::class, 'edit']);
-
-Route::resource('posts', PostController::class)->except(['index', 'show'])->middleware('auth');
-Route::resource('posts', PostController::class)->only(['index', 'show']);
+//Route::resource('posts', PostController::class)->except(['index', 'show'])->middleware('auth');
+//Route::resource('posts', PostController::class)->only(['index', 'show']);
 Route::resource('categories', CategoryController::class);
 
 Auth::routes();
