80e19a99944ef1f345f5a7d9b249406a5f48a7d8
VAdam02 <adam.vilanyi2@gmail.com>
2024-04-09 18:19:49 +0200

    modules intro

---
+++ HTMLDemo/script1.js
@@ -0,0 +1,6 @@
+const script1number = 5;
+
+console.log("script1", script1number)
+//console.log("script1", script2number)
+
+export default script1number;
---
+++ HTMLDemo/script2.js
@@ -0,0 +1,4 @@
+//const script1number = 5;
+import script1number from './script1.js';
+console.log("script2", script1number)
+//console.log("script2", script2number)
