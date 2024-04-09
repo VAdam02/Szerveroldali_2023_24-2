e43ae1c8a0e988765e4b9b089bbb3c614dcc70f7
VAdam02 <adam.vilanyi2@gmail.com>
2024-04-09 19:14:21 +0200

    Await

--- ModulesIntro/await.js
+++ ModulesIntro/await.js
@@ -2,7 +2,18 @@ const { readdir: readDir, readFile, writeFile } = require('fs/promises');
 
 //IIFE - Immediately Invoked Function Expression
 (
-    () => {
-        console.log('hello world');
+    async () => {
+        const names = await readDir('./files');
+
+        const output = [];
+        for(const name of names) {
+            const file = await readFile("./files/" + name, {encoding: 'utf-8'});
+            output.push(file);
+            console.log(file)
+        }
+
+        await writeFile('./concat.txt', output.join('\n'));
+
+        console.log('finished');
     }
 )();
