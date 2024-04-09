ae10095cb0484d644d759c9614a1e69bba12a42e
VAdam02 <adam.vilanyi2@gmail.com>
2024-04-09 19:06:00 +0200

    Promise chaining

--- ModulesIntro/promise.js
+++ ModulesIntro/promise.js
@@ -1,8 +1,6 @@
 const {readdir : readDir, readFile, writeFile} = require('fs/promises');
 
 readDir("./files")
-.then(names => {
-    Promise.all(names.map(name => readFile('./files/' + name, {encoding: 'utf-8'})))
-    .then(files => writeFile('./concat.txt', files.join('\n')))
-    .then(() => console.log('finished'))
-})
+.then(names => Promise.all(names.map(name => readFile('./files/' + name, {encoding: 'utf-8'}))))
+.then(files => writeFile('./concat.txt', files.join('\n')))
+.then(() => console.log('finished'))
