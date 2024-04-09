3dfd0f0816090fd8831fd9a4f11e5e57d7c3ef4b
VAdam02 <adam.vilanyi2@gmail.com>
2024-04-09 19:03:18 +0200

    Promise better

--- ModulesIntro/promise.js
+++ ModulesIntro/promise.js
@@ -10,18 +10,11 @@ readDir("./files")
     for (const name of names) {
         promises.push(
             readFile("./files/" + name, {encoding: 'utf-8'})
-            .then(file => {
-                output.push(file);
-                console.log(file);
-            })
         )
     }
 
     Promise.all(promises)
-    .then(() => {
-        writeFile('./concat.txt', output.join('\n'))
-        .then(() => {
-            console.log('finished');
-        });
+    .then(files => {
+        writeFile("./concat.txt", files.join("\n"))
     })
 })
