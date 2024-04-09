c9004a07afe41124bd5e1c3e3873bb78b787f6aa
VAdam02 <adam.vilanyi2@gmail.com>
2024-04-09 18:51:17 +0200

    Callback hell

--- ModulesIntro/callback.js
+++ ModulesIntro/callback.js
@@ -1,18 +1,27 @@
 const { readdir: readDir, readFile, writeFile } = require('fs');
 
 readDir('./files', (err, names) => {
+    //callack 1
     console.log(names);
 
     const output = [];
+    const finished = [];
 
     for (const name of names) {
         readFile('./files/' + name, { encoding: 'utf-8'}, (err, file) => {
+            //callback 2
             console.log(file);
             output.push(file);
+            finished.push(name);
+
+            if (finished.length == names.length) {
+                writeFile('./concat.txt', output.join('\n'), (err) => {
+                    //callback 3
+                    console.log('finished');
+
+                    //callback hell
+                })
+            }
         })
     }
-
-    writeFile('./concat.txt', output.join('\n'), (err) => {
-        console.log('finished');
-    })
 })
