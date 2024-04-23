5aade751753fcfb8439a0a4f62ec939bbffa8667
VAdam02 <adam.vilanyi2@gmail.com>
2024-04-23 18:15:42 +0200

    Get all

--- ModulesIntro/server.js
+++ ModulesIntro/server.js
@@ -2,8 +2,14 @@ const fastify = require('fastify') ({
     logger: true
 })
 
+const { User, Post, Category } = require("./models")
+
 fastify.get("/hello", async (request, reply) => {
     reply.send("Hello world")
+})
+
+fastify.get("/posts", async (request, reply) => {
+    reply.send(await Post.findAll())
 })
 
 fastify.listen({port: 3001}, (err, address) => {
