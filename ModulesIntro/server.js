0e3236b26d8ac0e95ab2e06c1f1e3fd1191e15ec
VAdam02 <adam.vilanyi2@gmail.com>
2024-04-23 18:19:53 +0200

    Get post

--- ModulesIntro/server.js
+++ ModulesIntro/server.js
@@ -12,6 +12,16 @@ fastify.get("/posts", async (request, reply) => {
     reply.send(await Post.findAll())
 })
 
+fastify.get("/posts/:id", async (request, reply) => {
+    const post = await Post.findByPk(request.params.id)
+
+    if (!post) {
+        reply.code(404).send({message: "Post not found"})
+    }
+
+    reply.send(post)
+})
+
 fastify.listen({port: 3001}, (err, address) => {
     if (err) throw err
 })
