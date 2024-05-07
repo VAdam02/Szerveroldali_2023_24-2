6a57ceae9b92a8cc702d64e631486c33630c7dd1
VAdam02 <adam.vilanyi2@gmail.com>
2024-05-07 18:47:06 +0200

    Model request

--- ModulesIntro/graphql/resolvers.js
+++ ModulesIntro/graphql/resolvers.js
@@ -1,3 +1,5 @@
+const { Category, Post, User } = require("../models");
+
 module.exports = {
     Query: {
         hello: async () => {
@@ -5,6 +7,29 @@ module.exports = {
             //return null //hiba lesz az eredmény
         },
         hello2: async (_, {name}) => `Hello ${name}`,
-        isEven: async (_, {num}) => num % 2 === 0
+        isEven: async (_, {num}) => num % 2 === 0,
+        categories: async () => {
+            return Category.findAll();
+        },
+        posts: async () => {
+            return Post.findAll();
+        }
+    },
+
+    Category: {
+        dummy: async () => {
+            return "dummy"
+        },
+        posts: async (category) => {
+            return category.getPosts();
+        }
+    },
+    Post: {
+        categories: async (post) => {
+            return post.getCategories();
+        },
+        user: async (post) => {
+            return post.getUser();
+        }
     }
 }
