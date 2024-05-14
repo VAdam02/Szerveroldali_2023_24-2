aaa1bff40a59b4747666fe4db96e03997541d706
VAdam02 <adam.vilanyi2@gmail.com>
2024-05-14 19:22:54 +0200

    Graphql 12 feladat

--- zh_kezdocsomag-main/graphql/resolvers.js
+++ zh_kezdocsomag-main/graphql/resolvers.js
@@ -16,6 +16,34 @@ module.exports = {
         classes: async () => {
             const students = await Student.findAll();
             return students.sort((a, b) => a.semester - b.semester || !a.classLetter.localeCompare(b.classLetter)).map(student => `${student.semester}. ${student.classLetter}`);
+        },
+
+        getGroupRelatedStudentsFromClass: async (_, { semester, classLetter }) => {
+            classLetter = classLetter.length == 1 ? classLetter.toUpperCase() : null;
+            semester = parseInt(semester);
+            semester = 12 >= semester && semester >= 1 ? semester : null;
+            if (!classLetter || !semester) {
+                throw new Error("Invalid className");
+            }
+
+            const students = await Student.findAll({
+                where: {
+                    semester,
+                    classLetter
+                },
+                include: {
+                    model: Group,
+                    through: {
+                        where: {
+                            status: "ACCEPTED"
+                        }
+                    },
+                    attributes: []
+                },
+                attributes: ["name"]
+            })
+
+            return students.map(student => student.name).sort();
         }
     },
 };
