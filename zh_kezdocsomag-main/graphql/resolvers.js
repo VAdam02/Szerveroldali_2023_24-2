f5f064568d74346ef548300541209057ef7a0c58
VAdam02 <adam.vilanyi2@gmail.com>
2024-05-14 19:06:45 +0200

    GraphQL 11 feladat

--- zh_kezdocsomag-main/graphql/resolvers.js
+++ zh_kezdocsomag-main/graphql/resolvers.js
@@ -3,16 +3,19 @@ const db = require("../models");
 const { Sequelize, sequelize } = db;
 const { ValidationError, DatabaseError, Op } = Sequelize;
 // TODO: Importáld a modelleket
-// const { /* modellek importálása itt */ } = db;
+const { Teacher, Student, Group } = db;
 
 module.exports = {
     Query: {
         // Elemi Hello World! példa:
-        helloWorld: () => "Hello World!",
+        //helloWorld: () => "Hello World!",
 
         // Példa paraméterezésre:
-        helloName: (_, { name }) => `Hello ${name}!`,
+        //helloName: (_, { name }) => `Hello ${name}!`,
 
-        // TODO: Dolgozd ki a további resolver-eket a schema-val összhangban
+        classes: async () => {
+            const students = await Student.findAll();
+            return students.sort((a, b) => a.semester - b.semester || !a.classLetter.localeCompare(b.classLetter)).map(student => `${student.semester}. ${student.classLetter}`);
+        }
     },
 };
