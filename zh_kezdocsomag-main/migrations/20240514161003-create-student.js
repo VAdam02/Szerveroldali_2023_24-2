70d60a3df111ec2a37369043003bd61630efea43
VAdam02 <adam.vilanyi2@gmail.com>
2024-05-14 18:22:47 +0200

    Modellek

---
+++ zh_kezdocsomag-main/migrations/20240514161003-create-student.js
@@ -0,0 +1,42 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      semester: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      classLetter: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Students');
+  }
+};
---
+++ zh_kezdocsomag-main/models/groupstudent.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class GroupStudent extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  GroupStudent.init({
+    status: DataTypes.ENUM('PENDING', 'ACCEPTED', 'REJECTED'),
+    GroupId: DataTypes.INTEGER,
+    StudentId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'GroupStudent',
+  });
+  return GroupStudent;
+};
